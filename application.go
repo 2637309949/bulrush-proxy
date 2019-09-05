3ea941f46a08df42b51ee013dc008dc042ea81df
double <2637309949@qq.com>
2019-09-05 23:28:27 +0800

    styles: 代码优化

--- application.go
+++ application.go
@@ -5,82 +5,22 @@
 package proxy
 
 import (
-	"bytes"
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"regexp"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 // Proxy http proxy
 type Proxy struct {
+	URL   string
 	Host  string
 	Match string
 	Map   func(string) string
+	req   func(*http.Request, *http.Request) *http.Request
+	res   func(gin.ResponseWriter, *http.Response)
 }
 
 // Plugin for gin
 func (proxy *Proxy) Plugin(httpProxy *gin.Engine) {
 	httpProxy.Use(middleware(proxy))
-}
-
-// proxy middleware
-func middleware(proxy *Proxy) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// MatchString url
-		if proxy.Match != "" {
-			r, _ := regexp.Compile(proxy.Match)
-			if !r.MatchString(c.Request.RequestURI) {
-				c.Next()
-				return
-			}
-		}
-		// Parse body
-		body, err := ioutil.ReadAll(c.Request.Body)
-		if err != nil {
-			http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
-		}
-
-		// Reassgin to body
-		c.Request.Body = ioutil.NopCloser(bytes.NewReader(body))
-		// Map path
-		var url string
-		if proxy.Map != nil {
-			url = proxy.Map(c.Request.RequestURI)
-		} else {
-			url = c.Request.RequestURI
-		}
-		url = fmt.Sprintf("%s%s", proxy.Host, url)
-
-		proxyReq, err := http.NewRequest(c.Request.Method, url, bytes.NewReader(body))
-		proxyReq.Header = make(http.Header)
-
-		// Reassgin headers
-		for key, val := range c.Request.Header {
-			proxyReq.Header[key] = val
-		}
-
-		// replace host
-		if proxy.Host != "" {
-			proxyReq.Header["Host"] = []string{string(proxy.Host[strings.Index(proxy.Host, "://")+3:])}
-		}
-
-		client := &http.Client{}
-		resp, err := client.Do(proxyReq)
-		if err != nil {
-			http.Error(c.Writer, err.Error(), http.StatusBadGateway)
-			return
-		}
-		defer resp.Body.Close()
-		bodyContent, _ := ioutil.ReadAll(resp.Body)
-		c.Status(resp.StatusCode)
-		for key, val := range resp.Header {
-			c.Writer.Header()[key] = val
-		}
-		c.Writer.Write(bodyContent)
-		c.Abort()
-	}
 }
---
+++ ginproxy.go
@@ -0,0 +1,118 @@
+// Copyright (c) 2018-2020 Double All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package proxy
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	nurl "net/url"
+	opath "path"
+	"regexp"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func resolve(path string, proxy *Proxy) string {
+	url := path
+	if proxy.URL != "" {
+		url = proxy.URL
+		r, _ := regexp.Compile("^http")
+		if !r.MatchString(url) {
+			if proxy.Host != "" {
+				u, _ := nurl.Parse(proxy.Host)
+				u.Path = opath.Join(u.Path, url)
+				url = u.String()
+			}
+			url = ""
+		}
+		return url
+	}
+	if proxy.Map != nil {
+		url = proxy.Map(url)
+	}
+	url = strings.Split(url, "?")[0]
+	if proxy.Host != "" {
+		u, _ := nurl.Parse(proxy.Host)
+		u.Path = opath.Join(u.Path, url)
+		return u.String()
+	}
+	return ""
+}
+
+func request(req *http.Request) (*http.Response, error) {
+	client := &http.Client{}
+	return client.Do(req)
+}
+
+func middleware(proxy *Proxy) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		url := resolve(c.Request.URL.Path, proxy)
+		if url == "" {
+			c.Next()
+			return
+		}
+
+		// MatchString url
+		if proxy.Match != "" {
+			r, _ := regexp.Compile(proxy.Match)
+			if !r.MatchString(c.Request.RequestURI) {
+				c.Next()
+				return
+			}
+		}
+
+		// Parse body
+		body, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
+		}
+
+		// Reassgin to body
+		c.Request.Body = ioutil.NopCloser(bytes.NewReader(body))
+
+		url = fmt.Sprintf("%s?%s", url, c.Request.URL.RawQuery)
+
+		proxyReq, err := http.NewRequest(c.Request.Method, url, bytes.NewReader(body))
+		proxyReq.Header = make(http.Header)
+
+		// Reassgin headers
+		for key, val := range c.Request.Header {
+			proxyReq.Header[key] = val
+		}
+
+		// replace host
+		if proxy.Host != "" {
+			proxyReq.Header["Host"] = []string{string(proxy.Host[strings.Index(proxy.Host, "://")+3:])}
+		}
+
+		// custom proxyReq if you need
+		if proxy.req != nil {
+			proxyReq = proxy.req(proxyReq, c.Request)
+		}
+
+		resp, err := request(proxyReq)
+		if err != nil {
+			http.Error(c.Writer, err.Error(), http.StatusBadGateway)
+			return
+		}
+		defer resp.Body.Close()
+		bodyContent, _ := ioutil.ReadAll(resp.Body)
+		c.Status(resp.StatusCode)
+		for key, val := range resp.Header {
+			c.Writer.Header()[key] = val
+		}
+
+		// custom proxyRes if you need
+		if proxy.res != nil {
+			proxy.res(c.Writer, resp)
+		}
+
+		c.Writer.Write(bodyContent)
+		c.Abort()
+	}
+}
